6fc432c4c0eddd29ef6e27e16002585ea6d2dcb8
To-Ev <aremuakinteye@gmail.com>
2026-04-06 13:29:17 +0100

    modified:   src/component/Common/Navbar.jsx 	modified:   src/component/Layout/Hero.jsx

--- src/component/Common/Navbar.jsx
+++ src/component/Common/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
             </div>
             {/* Right Icons */}
             <div className='flex items-center space-x-4'>
-                <Link to="/admin" className='block bg-black rounded text-sm px-2 text-white'>Admin</Link>
+                <Link to="/admin" className='block bg-black rounded text-sm px-3 py-1 text-white'>Admin</Link>
                 <Link to='/profile' className='hover:text-black'>
                     <HiOutlineUser className='h-6 w-6 text-gray-700'/>
                 </Link>
--- src/component/Layout/Hero.jsx
+++ src/component/Layout/Hero.jsx
@@ -3,21 +3,21 @@ import rabbitImg from '../../assets/Hero-secImg.jpg'
 import { Link } from 'react-router-dom'
 const Hero = () => {
   return (
-    <section className='relative'>
-        <img src={rabbitImg} alt="rabbitImg" 
-        className='w-full h-[100] md:h-[150] lg:h-[190]'/>
-        <div 
-        className='absolute inset-0 flex justify-center items-center bg-black/10'>
-            <div className='text-center text-white p-6'>
-              <h1 className='text-4xl md:text-9xl font-bold uppercase tracking-tighter mb-2'>
-                Ready To <br/>Wear
-              </h1>
-              <p 
-              className='text-sm tracking-tighter md:text-lg mb-4'>Explore our ready to wear outfits with Nation wide shipping
-              </p>
-              <Link to="#" className='px-6 py-3 rounded-md font-semibold bg-white text-gray-900'>Shop Now</Link>
-            </div>
+    <section className='relative min-h-full'>
+      <img src={rabbitImg} alt="rabbitImg" 
+      className='w-full h-full object-cover'/>
+      <div 
+      className='absolute inset-0 flex justify-center items-center bg-black/10'>
+        <div className='text-center text-white p-6'>
+          <h1 className='text-4xl sm:text-6xl md:text-9xl font-bold uppercase tracking-tighter mb-2'>
+            Ready To <br/>Wear
+          </h1>
+          <p 
+          className='text-sm tracking-tighter md:text-lg mb-4'>Explore our ready to wear outfits with Nation wide shipping
+          </p>
+          <Link to="#" className='px-6 py-3 rounded-md font-semibold bg-white text-gray-900'>Shop Now</Link>
         </div>
+      </div>
     </section>
   )
 }
